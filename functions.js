2ec976dc79de7e22d4a652aab6eefaf0958e1e10
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2021-08-29 16:20:16 -0300

    Adicionando mais casos de teste :3

--- functions.js
+++ functions.js
@@ -1,24 +1,9 @@
-// function solucaoPrimeiroGrau(a, b) {
-//     if (a == 0) break;
-//     // let result = { x: -b / a };
-//     return result;
-// }
-// function solucaoSegundoGrau(a, b, c) {
-//     if (a == 0) break;
-//     var soma = -b / a;
-//     var produto = c / a;
-//     var x1, x2;
-//     if (x1 + x2 == soma && x1 * x2 == produto) {
-//         let result = { x1: x1, x2: x2 };
-//         return result;
-//     }
-// }
 const functions = {
     solucaoPrimeiroGrau: (a, b) => a != 0 ? -b / a : 'erro',
     solucaoSegundoGrau: (a, b, c) => {
         var x1, x2;
         var raizes = {};
-        const delta = b * b - 4 * a * c;
+        const delta = (b * b) - (4 * a * c);
         if (a == 0)
             return raizes = { x1: 'erro', x2: 'erro' };
         else
@@ -27,8 +12,8 @@ const functions = {
                 x2 = x1;
                 return raizes = { x1: x1, x2: x2 };
             } else if (delta > 0) {
-                x1 = -b + Math.sqrt(delta) / (2 * a);
-                x2 = -b - Math.sqrt(delta) / (2 * a);
+                x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                x2 = (-b - Math.sqrt(delta)) / (2 * a);
                 return raizes = { x1: x1, x2: x2 };
             } else {
                 x1 = 'delta negativo';
--- functions.test.js
+++ functions.test.js
@@ -1,11 +1,53 @@
 const functions = require('./functions');
 
-// test('Verificar funções', () => {
-//     expect(functions.solucaoPrimeiroGrau(-1,4)).toBe(-1);
-// })
+test('Teste função primeiro grau 1', () => {
+    expect(functions.solucaoPrimeiroGrau(-1, 4)).toBe(4);
+})
+test('Teste função primeiro grau 2', () => {
+    expect(functions.solucaoPrimeiroGrau(-1, 0)).toBe(0);
+})
+test('Teste função primeiro grau 3 (falha)', () => {
+    expect(functions.solucaoPrimeiroGrau(9, 3.5)).toBe(-3.5 / 2);
+})
+test('Teste função primeiro grau 4 (falha)', () => {
+    expect(functions.solucaoPrimeiroGrau(-10, 6)).toBe(-0.6);
+})
+test('Teste função primeiro grau 5', () => {
+    expect(functions.solucaoPrimeiroGrau(-1, -5.5)).toBe(-5.5);
+})
+test('Teste função primeiro grau: a=0', () => {
+    expect(functions.solucaoPrimeiroGrau(0, 4)).toBe('erro');
+})
 
-test('Verificar funções2', () => {
-    const resultado = functions.solucaoSegundoGrau(0, 1, -4)
-    expect(resultado.x1).toBe('erro')
-    expect(resultado.x2).toBe('erro')
+//testes da função do segundo grau
+
+test('Teste função do segundo grau 1', () => {
+    const result = functions.solucaoSegundoGrau(1, -1, 6);
+    expect(result.x1).toBe('delta negativo')
+    expect(result.x2).toBe('delta negativo')
+})
+test('Teste função do segundo grau 2 (falha)', () => {
+    const result = functions.solucaoSegundoGrau(1, 3, 9);
+    expect(result.x1).toBe(1)
+    expect(result.x2).toBe(-5)
+})
+test('Teste função do segundo grau 3', () => {
+    const result = functions.solucaoSegundoGrau(1, 5, 6);
+    expect(result.x1).toBe(-2)
+    expect(result.x2).toBe(-3)
+})
+test('Teste função do segundo grau 4', () => {
+    const result = functions.solucaoSegundoGrau(1, 1, 0);
+    expect(result.x1).toBe(0)
+    expect(result.x2).toBe(-1)
+})
+test('Teste função do segundo grau 5', () => {
+    const result = functions.solucaoSegundoGrau(1, 0, -9);
+    expect(result.x1).toBe(3)
+    expect(result.x2).toBe(-3)
+})
+test('Teste função do segundo grau: a=0 (falha)', () => {
+    const result = functions.solucaoSegundoGrau(0, -1, 6);
+    expect(result.x1).toBe(1)
+    expect(result.x2).toBe(-1)
 })
