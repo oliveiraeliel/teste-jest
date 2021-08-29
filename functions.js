98dec70a40d6022c46b585a4b53917ffb516520d
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2021-08-29 14:55:49 -0300

    Atualização

--- functions.js
+++ functions.js
@@ -14,8 +14,27 @@
 //     }
 // }
 const functions = {
-    solucaoPrimeiroGrau: (a, b) => -b/a
-    // solucaoSegundoGrau: (a, b, c) => 
+    solucaoPrimeiroGrau: (a, b) => a != 0 ? -b / a : 'erro',
+    solucaoSegundoGrau: (a, b, c) => {
+        var x1, x2;
+        var raizes = {};
+        const delta = b * b - 4 * a * c;
+        if (a == 0)
+            return raizes = { x1: 'erro', x2: 'erro' };
+        else
+            if (delta == 0) {
+                x1 = (-b) / (2 * a);
+                x2 = x1;
+                return raizes = { x1: x1, x2: x2 };
+            } else if (delta > 0) {
+                x1 = -b + Math.sqrt(delta) / (2 * a);
+                x2 = -b - Math.sqrt(delta) / (2 * a);
+                return raizes = { x1: x1, x2: x2 };
+            } else {
+                x1 = 'delta negativo';
+                x2 = x1;
+                return raizes = { x1: x1, x2: x2 };
+            }
+    }
 }
 module.exports = functions;
-// if (a == 0) return 'a não pode ser igual à 0'
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,11 @@
 const functions = require('./functions');
 
-test('Verificar funções', () => {
-    expect(functions.solucaoPrimeiroGrau(-1,4)).toBe(-1);
+// test('Verificar funções', () => {
+//     expect(functions.solucaoPrimeiroGrau(-1,4)).toBe(-1);
+// })
+
+test('Verificar funções2', () => {
+    const resultado = functions.solucaoSegundoGrau(0, 1, -4)
+    expect(resultado.x1).toBe('erro')
+    expect(resultado.x2).toBe('erro')
 })
